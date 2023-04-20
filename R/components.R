7952553e3c9d8da428b706693445d8115c618880
OganM <ogan.mancarci@gmail.com>
2023-04-19 23:32:33 -0700

    alternative replacedBy encoding

--- R/components.R
+++ R/components.R
@@ -10,7 +10,7 @@ process_ontology = function(onto_xml){
              imports =
                  ontology_elements$imports %>% xml2::xml_attr('resource'),
              default_namespace = ontology_elements$`default-namespace` %>% xml2::xml_contents() %>% as.character(),
-             source = onto_xml
+             source = as.character(onto_xml)
         )
 
     return(ontology)
@@ -20,9 +20,14 @@ process_ontology = function(onto_xml){
 process_class = function(class_xml){
     class_elements <- get_elements(class_xml)
 
+    replacement = c(class_elements$IAO_0100001 %>% xml2::xml_attr('resource'),
+                    class_elements$IAO_0100001 %>% xml2::xml_text()) %>% na.omit() %>% {.[.!='']} %>% unique
 
-    Class = list(IRI = x %>% xml2::xml_attr('about'),
+
+    Class = list(IRI = class_xml %>% xml2::xml_attr('about'),
                  Definition = class_elements$IAO_0000115 %>% xml2::xml_text(),
                  AlternativeIds = class_elements$hasAlternativeId %>% xml2::xml_text(),
-                 source = class_xml)
+                 ReplacedBy = replacement,
+                 deprecated = class_elements$deprecated %>% xml2::xml_text() %>% {ifelse(length(.)!=0 && . =='true',TRUE,FALSE)},
+                 source = as.character(class_xml))
 }
